44b5cc12f335f735623d76704796f24fe539d470
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-07-14 13:59:18 +0200

    first commit

---
+++ src/components/task.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const App = () => {
+  const handleClick = () => {
+    display 
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>
+        Add Task
+      </button>
+    </div>
+  );
+};
+
+export default App;
